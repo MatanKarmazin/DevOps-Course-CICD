ebed96735c221b2c8e5259f9dd6653ccac417f5e
MatanKarmazin <karmazin.matan@gmail.com>
2026-04-20 09:48:22 +0300

    Changed MatanKarmazin.java

--- MatanKarmazin.java
+++ MatanKarmazin.java
@@ -1,7 +1,7 @@
 public class MatanKarmazin {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Matan Karmazin ... " + i);
+            System.out.println("My name is Matan Karmazin ... " + i);
         }
     }
 }
